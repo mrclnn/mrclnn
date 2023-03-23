16064af6af54d4a193e376cc3d202a6f3de6fd94
mrclnn <boris.2021@mail.ru>
2023-03-23 23:22:27 +0300

    refactoring aggregator->model relates for tags

--- app/GalleryTagAggregator.php
+++ app/GalleryTagAggregator.php
@@ -6,36 +6,15 @@ use Illuminate\Support\Facades\DB;
 
 class GalleryTagAggregator
 {
-    public static array $allTags;
-    public static function getAllTags() : array
-    {
-        if(isset(self::$allTags)) return self::$allTags;
-        $query = <<<QUERY
-select
-id
-from tags
-QUERY;
-        $allTagsIds = DB::select($query);
-        foreach($allTagsIds as $tagId){
-            $tag = new GalleryTagModel();
-            $tag->getTagFromId($tagId->id);
-            self::$allTags[] = $tag;
-        }
-        return self::$allTags;
-
-    }
-
     public static function searchTag(string $searchWord) : ?array
     {
         $query = <<<QUERY
-select id from tags where tag like '%$searchWord%'
+select * from tags where tag like '%$searchWord%'
 QUERY;
-        $matchedTags = DB::select($query);
+        $tagsData = DB::select($query);
         $tags = [];
-        foreach($matchedTags as $matchedTag){
-            $tag = new GalleryTagModel();
-            $tag->getTagFromId($matchedTag->id);
-            $tags[] = $tag;
+        foreach($tagsData as $tagData){
+            $tags[] = (new GalleryTagModel())->fillFromData($tagData);
         }
         return $tags;
 
@@ -45,20 +24,13 @@ QUERY;
     {
         if(empty($idList)) return null;
         $idList = implode(',', $idList);
-        $query = "select
-    id,
-    tag,
-    type,
-    count,
-    enabled
+        $query = "select *
 from tags
 where id in ($idList)";
         $tagsInfo = DB::select($query, [$idList]);
         $tags = [];
         foreach($tagsInfo as $tagInfo){
-            $tag = new GalleryTagModel();
-            $tag->getFromData($tagInfo);
-            $tags[] = $tag;
+            $tags[] = (new GalleryTagModel())->fillFromData($tagInfo);
         }
         return $tags;
 
--- app/GalleryTagModel.php
+++ app/GalleryTagModel.php
@@ -11,39 +11,8 @@ class GalleryTagModel
     public string $tag;
     public int $count;
     public bool $enabled;
-    public function getTagFromName(string $name) : ?GalleryTagModel
-    {
-        $query = <<<QUERY
-select
-id,
-type,
-tag,
-count,
-enabled
-from tags
-where tag = ?
-QUERY;
-        $params = [$name];
-        return $this->getTag($query, $params);
-    }
-    public function getTagFromId(int $id) : ?GalleryTagModel
-    {
-        $query = <<<QUERY
-select
-id,
-type,
-tag,
-count,
-enabled
-from tags
-where id = ?
-QUERY;
-        $params = [$id];
-        return $this->getTag($query, $params);
 
-    }
-
-    public function getFromData(object $data) : GalleryTagModel
+    public function fillFromData(object $data) : GalleryTagModel
     {
         // подразумевается что object $data это полученное из базы данных представление в Aggregator
         $this->id = $data->id ?? null;
@@ -54,22 +23,5 @@ QUERY;
 
         return $this;
     }
-
-
-
-    private function getTag(string $query, array $params) : ?GalleryTagModel
-    {
-        $res = DB::select($query, $params);
-        if(empty($res)) return null;
-
-        $this->id = (int)$res[0]->id;
-        $this->type = (string)$res[0]->type;
-        $this->tag = (string)$res[0]->tag;
-        $this->count = (int)$res[0]->count;
-        $this->enabled = (bool)$res[0]->enabled;
-
-        return $this;
-    }
-
 
 }
