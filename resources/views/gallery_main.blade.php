bfa807f3fd2a959525e688119b0d15fa32751921
mrclnn <boris.2021@mail.ru>
2023-03-31 22:19:17 +0300

    update interface

--- resources/views/gallery_main.blade.php
+++ resources/views/gallery_main.blade.php
@@ -15,8 +15,16 @@
 
 </head>
 <body>
-<h1>this is main gallery page</h1>
-<div id="add-category-frame">
+<h1>Tags config</h1>
+<div id="tags-config" class="main-frame">
+    <input type="text" name="search-tag" id="search-tag" placeholder="search tag">
+    <ul id="selected-tags"></ul>
+    <span id="tag-alias"></span>
+    <input type="text" name="search-alias" id="search-alias">
+</div>
+<h1>Categories config</h1>
+<div id="add-category-frame" class="main-frame">
+    <h2 class="main-frame-title">Add new category</h2>
     <input type="text" name="add-category-name" id="add-category-name" placeholder="name of category">
 
     <input type="text" class="search-tag" id="extend-search" placeholder="EXTEND TAGS">
@@ -31,16 +39,32 @@
     <ul class="selected-tags" id="included-tags"></ul>
 
     <input type="button" name="clear" id="clear" value="clear">
-    <input type="button" name="add-category" id="add-category" value="add category">
+    <input type="button" name="add-category" id="add-category" value="add category" data-target="create">
 </div>
-<div id="categories-container">
-
+<div id="categories-container" class="main-frame">
+    <h2 class="main-frame-title">Categories list</h2>
     @if(count($categories) > 0)
         @foreach($categories as $category)
-            <div class="category" data-id="{{$category->id}}">
-                <span class="category-name">{{$category->name}}</span>
+            <div class="category main-frame" data-id="{{$category->id}}">
                 <span class="category-count">{{$category->count}}</span>
-                <input type="button" class="update-category" value="update category">
+                <span class="category-name">{{$category->name}}</span>
+                <ul class="extend-tags hidden">
+                    @foreach($category->extendTags as $tag)
+                        <li class="selectedTag" data-id="{{$tag->id}}">{{$tag->tag}}</li>
+                    @endforeach
+                </ul>
+                <ul class="exclude-tags hidden">
+                    @foreach($category->excludeTags as $tag)
+                        <li class="selectedTag" data-id="{{$tag->id}}">{{$tag->tag}}</li>
+                    @endforeach
+                </ul>
+                <ul class="include-tags hidden">
+                    @foreach($category->includeTags as $tag)
+                        <li class="selectedTag" data-id="{{$tag->id}}">{{$tag->tag}}</li>
+                    @endforeach
+                </ul>
+                <input type="button" class="edit-category" value="edit category">
+                <input type="button" class="update-category" value="recount category">
                 <input type="button" class="delete-category" value="delete category">
             </div>
         @endforeach
